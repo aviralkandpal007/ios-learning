82db39071c6f73407f72978aa27aba70be22292c
Aviral Kandpal <aviralkandpal007@gmail.com>
2025-09-10 08:59:14 +0530

    Student App Sync

---
+++ StudentApp/StudentApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudentApp
+//
+//  Created by Aviral Kandpal on 10/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
